633ea04a5d76b6183ea6316aed3c756c310868a1
Jeff Wang <jeffawang@gmail.com>
2017-12-09 20:26:03 -0800

    implement hints, etc.

--- types.go
+++ types.go
@@ -11,18 +11,24 @@ const (
 )
 
 var (
-	ErrOutOfTurn  = fmt.Errorf("player played card out of turn")
-	ErrOutOfBombs = fmt.Errorf("Out of Bombs")
-	ErrGameOver   = fmt.Errorf("Game ended")
+	ErrOutOfTurn   = fmt.Errorf("player played card out of turn")
+	ErrOutOfBombs  = fmt.Errorf("Out of Bombs")
+	ErrGameOver    = fmt.Errorf("Game ended")
+	ErrNoHintsLeft = fmt.Errorf("No hints remaining.")
 )
 
 // TODO: represent hints (struct?)
+
+type Hints struct {
+	ColorHints  byte
+	NumberHints byte
+}
 
 // Board holds game state
 type Board struct {
 	Piles              map[Color]Number
 	Deck               []*Card
-	Discard            []*Card
+	DiscardPile        []*Card
 	Players            []*Player
 	CurrentPlayerIndex int
 	Hints              int
@@ -50,14 +56,35 @@ func (b *Board) card(pi, ci int) *Card {
 	player := b.Players[pi]
 	card := player.Hand[ci]
 	copy(player.Hand[ci:], player.Hand[ci+1:])
-	newCard, ok := b.drawCard()
+	newCard := b.drawCard()
 	player.Hand[len(player.Hand)-1] = newCard
 	return card
 }
 
-func (b *Board) GiveHint(pi int, ci int) error {
+func (b *Board) GiveHint(pi int, ci int, numhint bool) error {
 	if !b.decrementHint() {
 		return ErrNoHintsLeft
+	}
+
+	p := b.Players[pi]
+	c := p.Hand[ci]
+
+	if numhint {
+		for _, card := range p.Hand {
+			var mask byte = 1 << uint(c.N)
+			if card.N != c.N {
+				mask = ^mask
+			}
+			c.NumberHints &= mask
+		}
+	} else {
+		for _, card := range p.Hand {
+			var mask byte = 1 << uint(c.C)
+			if card.C != c.C {
+				mask = ^mask
+			}
+			c.ColorHints &= mask
+		}
 	}
 	return nil
 }
@@ -76,7 +103,7 @@ func (b *Board) Play(pi int, ci int) error {
 		}
 	} else {
 		b.Bombs--
-		b.Discard = append(b.Discard, card)
+		b.DiscardPile = append(b.DiscardPile, card)
 		if b.Bombs <= 0 {
 			return ErrOutOfBombs
 		}
@@ -93,7 +120,7 @@ func (b *Board) Discard(pi int, ci int) error {
 	}
 	card := b.card(pi, ci)
 	b.incrementHint()
-	b.Discard = append(b.Discard, card)
+	b.DiscardPile = append(b.DiscardPile, card)
 	if len(b.Deck) == 0 {
 		return ErrGameOver
 	}
@@ -117,7 +144,6 @@ func (b *Board) incrementHint() {
 func (b *Board) drawCards(n int) []*Card {
 	var cards []*Card
 	for i := 0; i < n; i++ {
-		card := b.drawCard()
 		cards = append(cards, b.drawCard())
 	}
 	return cards
@@ -139,6 +165,10 @@ func newDeck() []*Card {
 				deck = append(deck, &Card{
 					C: Color(color),
 					N: Number(num),
+					Hints: Hints{
+						ColorHints:  0x3E,
+						NumberHints: 0x3E,
+					},
 				})
 			}
 		}
@@ -173,6 +203,7 @@ type Number int
 type Card struct {
 	C Color  `json:"c"`
 	N Number `json:"n"`
+	Hints
 }
 
 // Player represents a player
